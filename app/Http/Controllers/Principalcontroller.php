b0afefc6d30dab7ba6eea67df53c1e09202578e7
jao <joaogabrielzx884@gmail.com>
2025-09-03 11:26:29 -0300

    ultimo commit

---
+++ app/Http/Controllers/Principalcontroller.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class Principalcontroller extends Controller
+{
+    public function bakery(){
+        return view('bakery');
+    }
+}
